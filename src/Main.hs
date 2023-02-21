9dcab4f37833e07cc95a0ddc66e3bc639a432fc9
Blek <egorka99966@gmail.com>
2023-02-21 09:07:38 +0700

    fixed rays and blending colors

--- src/Main.hs
+++ src/Main.hs
@@ -17,21 +17,20 @@ viewport_width = aspect_ratio * viewport_height
 focal_length = 1
 
 color_ray :: Ray -> Color
-color_ray ray = blend white blue ((/ viewport_height) . (+ viewport_height/2) . y . norm . dir $ ray)
+color_ray ray = blend white blue ((/ viewport_height) . (+ viewport_height/2) . y . dir $ ray)
 
 write_file :: String -> [Color] -> IO ()
 write_file filename colors = withFile filename WriteMode (\handle -> do
     hPutStrLn handle "P3"
     hPutStrLn handle $ (show . floor $ image_width) ++ " " ++ (show . floor $ image_height)
     hPutStrLn handle "255"
-    mapM_ (hPrint handle) (map make_valid colors)
+    mapM_ (hPrint handle) colors
                                                          )
-    
 
 main :: IO ()
 main = do
     let viewport_left_corner = Vc3 (-viewport_width/2) (-viewport_height/2) (-focal_length)
-    let rays = [Ry zero (viewport_left_corner + u *>> forward + v *>> up) |
+    let rays = [Ry zero (viewport_left_corner + (u * viewport_width) *>> forward + (v * viewport_height) *>> up) |
             v <- [0, 1/(image_height - 1)..1],
             u <- [0, 1/(image_width - 1)..1]]
     let colors = map color_ray rays
