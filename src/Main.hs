be585a378e38776caaa2121641afe758c80de275
Kirill <k.romanov1@g.nsu.ru>
2023-03-07 12:10:23 +0700

    added defuse materials

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,7 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use camelCase" #-}
-
 module Main where
 
 import System.IO
 import System.Random
-import Data.Maybe
 
 import Vec3
 import Ray
@@ -25,11 +21,13 @@ viewport_width = aspect_ratio * viewport_height
 
 focal_length = 1
 
-color_ray :: Hittable a => a -> Ray -> Color
-color_ray objs ray = maybe background map_normal normal
+color_ray :: Hittable a => Int -> a -> Ray -> Color
+color_ray depth objs ray 
+    | depth < 10 = maybe background map_hit hit
+    | otherwise = Cl zero
     where 
-        map_normal normal = make_shadow (Vc3 1 0.3 (-1)) normal (Cl ((255 * 0.5) *>> (normal + one)))
-        normal = hit_normal ray (0.0001, 100) objs
+        map_hit (point, normal) = blend (color_ray (depth + 1) objs (Ry point (normal + randomVec3_in_sphere (randomInt depth)))) black (0.5)
+        hit = hit_data ray (0,100) objs
         background = blend blue white ((/ viewport_height) . (+ viewport_height/2) . y . norm . dir $ ray)
 
 write_file :: String -> [Color] -> IO ()
@@ -42,19 +40,19 @@ write_file filename colors = withFile filename WriteMode (\handle -> do
 
 main :: IO ()
 main = do
-    let samples_per_pixel = 50
-    let sphere = Sph (Vc3 0 0.2 (-1.5)) 0.5
-    let sphere2 = Sph (Vc3 0 (-100.5) 0) 100
+    let samples_per_pixel = 100
+    let sphere = Sph (Vc3 0 0.2 (-1.5)) (0.5)
+    let sphere2 = Sph (Vc3 0 (-100.5) 0) (100)
     let viewport_left_corner = Vc3 (-viewport_width/2) (-viewport_height/2) (-focal_length)
-    let rays = [(\(Cl vec) -> Cl (vec <<\ samples_per_pixel)) (multi_color u v (floor samples_per_pixel))|
+    let rays = map (\(Cl vec) -> (Cl (vec <<\ samples_per_pixel))) [multi_color u v (floor samples_per_pixel)|
             v <-  reverse [0, 1/(image_height - 1)..1],
             u <-  [0, 1/(image_width - 1)..1]]
                 where multi_color u v 0 = Cl zero
-                      multi_color u v n = color_ray [sphere, sphere2]
+                      multi_color u v n = color_ray 0 [sphere, sphere2] 
                         (Ry zero (
                             viewport_left_corner 
-                            + forward <<* (u * viewport_width + randomDbl (2*n::Int) / (image_width - 1))
-                            + up <<* (v * viewport_height + randomDbl (2*n-1::Int) / (image_height - 1))
+                            + forward <<* (u * viewport_width + (randomDbl (2*n::Int)) / (image_width - 1))
+                            + up <<* (v * viewport_height + (randomDbl (2*n-1::Int)) / (image_height - 1))
                                  )
                         ) <++>  multi_color u v (n - 1)
     write_file "output.ppm" rays
--- src/MyRandom.lhs
+++ src/MyRandom.lhs
@@ -3,8 +3,17 @@ module MyRandom where
 
 import System.IO
 import System.Random
+import Vec3
 
+randomInt :: Int -> Int
+randomInt seed = fst $ random $ mkStdGen seed
 
 randomDbl :: Int -> Double
 randomDbl seed = fst $ randomR (-1, 1) $ mkStdGen seed
+
+randomVec3 :: Int -> Vec3
+randomVec3 seed = Vc3 (randomDbl seed) (randomDbl $ seed * 2 - 1) (randomDbl $ seed * 2)
+
+randomVec3_in_sphere :: Int -> Vec3
+randomVec3_in_sphere seed = head $ filter (\vec -> length_sqr vec <= 1 ) [randomVec3 new_seed | new_seed <- [(randomInt seed)..]]
 \end{code}
