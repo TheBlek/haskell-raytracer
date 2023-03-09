476f509f95abdeddd1bc52ce3d89b4165482a863
Blek <egorka99966@gmail.com>
2023-03-09 19:45:44 +0700

    rewrote random to state stdgen. ray randomized properly now

--- src/Main.hs
+++ src/Main.hs
@@ -16,36 +16,44 @@ import MyRandom
 
 
 aspect_ratio = 16 / 9
-image_height = 720
+image_height = 360
 image_width = aspect_ratio * image_height
 
 viewport_height = 2
 viewport_width = aspect_ratio * viewport_height
 
-focal_length = 1
-{-
-color_ray :: Hittable a => Int -> a -> Ray -> Color
-color_ray depth objs ray 
-    | depth < 10 = maybe background map_hit hit
-    | otherwise = Cl zero
-    where 
-        map_hit (point, normal) = blend (color_ray (depth + 1) objs (Ry point (normal + randomVec3_in_sphere depth))) black 0.5
-        hit = hit_data ray (0.0001, 100) objs
-        background = blend light_blue white ((/ viewport_height) . (+ viewport_height/2) . y . norm . dir $ ray)
--}
+viewport_left_corner = Vc3 (-viewport_width/2) (-viewport_height/2) (-focal_length)
 
-color_ray :: Hittable a => Int -> a -> Ray -> State Int Color
+focal_length = 1
+
+color_ray :: Hittable a => Int -> a -> Ray -> State StdGen Color
 color_ray depth objs ray 
-    | depth >= 20 = return $ Cl zero
+    | depth >= 10 = return $ Cl zero
     | otherwise = do
-        seed <- get
-        let color (point, normal) = color_ray (depth + 1) objs (Ry point (normal + randomVec3_in_sphere seed))
+        offset <- random_vec_in_sphereS
+
+        let color (point, normal) = color_ray (depth + 1) objs (Ry point (normal + offset))
         let map_hit hit = (\clr -> blend clr black 0.5) <$> color hit
-        put $ randomInt seed 
+        let background = blend light_blue white ((/ viewport_height) . (+ viewport_height/2) . y . norm . dir $ ray)
+        let hit = hit_data ray (0.0001, 100) objs
+
         maybe (return background) map_hit hit
-        where 
-            background = blend light_blue white ((/ viewport_height) . (+ viewport_height/2) . y . norm . dir $ ray)
-            hit = hit_data ray (0.0001, 100) objs
+
+gen_ray :: Double -> Double -> State StdGen Ray
+gen_ray u v = do
+    u_offset <- randomRS (0, 1)
+    v_offset <- randomRS (0, 1)
+    return $ Ry zero (
+        viewport_left_corner 
+        + forward <<* (u + u_offset / (image_width - 1)) <<* viewport_width
+        + up <<* (v + v_offset / (image_height - 1)) <<* viewport_height
+                     )
+
+multi_color :: (Hittable a) => a -> Double -> Double -> Int -> State StdGen Color
+multi_color _ _ _ 0 = return black
+multi_color objs u v n = (<++>) 
+    <$> multi_color objs u v (n - 1) 
+    <*> (gen_ray u v >>= color_ray 0 objs)
 
 write_file :: String -> [Color] -> IO ()
 write_file filename colors = withFile filename WriteMode (\handle -> do
@@ -60,19 +68,12 @@ main = do
     let samples_per_pixel = 100
     let sphere = Sph (Vc3 0 0.2 (-1.5)) 0.5
     let sphere2 = Sph (Vc3 0 (-100.5) 0) 100
-    let viewport_left_corner = Vc3 (-viewport_width/2) (-viewport_height/2) (-focal_length)
-    let rays = [(\(Cl vec) -> Cl (vec <<\ samples_per_pixel)) $ multi_color u v (floor samples_per_pixel)|
-            v <-  reverse [0, 1/(image_height - 1)..1],
+    let objs = [sphere, sphere2]
+    let accumulated_color = [multi_color objs u v (floor samples_per_pixel)|
+            v <-  reverse [0, 1/(image_height - 1)..1], 
             u <-  [0, 1/(image_width - 1)..1]]
-                where multi_color u v 0 = Cl zero
-                      multi_color u v n = evalState (color_ray 0 [sphere, sphere2] 
-                        (Ry zero (
-                            viewport_left_corner 
-                            + forward <<* (u * viewport_width + randomDbl (2*n::Int) / (image_width - 1))
-                            + up <<* (v * viewport_height + randomDbl (2*n-1::Int) / (image_height - 1))
-                                 )
-                        )) n <++>  multi_color u v (n - 1)
-    write_file "output.ppm" rays
+    let colors = mapM (fmap (\(Cl x) -> Cl (x <<\ samples_per_pixel))) accumulated_color
+    write_file "output.ppm" $ evalState colors (mkStdGen 0)
  
     return ()
     
--- src/MyRandom.lhs
+++ src/MyRandom.lhs
@@ -1,9 +1,12 @@
 \begin{code}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
 module MyRandom where
 
 import System.IO
 import System.Random
 import Vec3
+import Control.Monad.State
 
 randomInt :: Int -> Int
 randomInt seed = fst $ random $ mkStdGen seed
@@ -16,4 +19,26 @@ randomVec3 seed = Vc3 (randomDbl seed) (randomDbl $ seed * 2 - 1) (randomDbl $ s
 
 randomVec3_in_sphere :: Int -> Vec3
 randomVec3_in_sphere seed = head $ filter (\vec -> length_sqr vec <= 1 ) [randomVec3 new_seed | new_seed <- [(randomInt seed)..]]
+
+randomS :: (Random a) => State StdGen a
+randomS = do
+    (x, g) <- gets random
+    put g
+    return x
+
+randomRS :: (Random a) => (a, a) -> State StdGen a
+randomRS range = do
+    (x, g) <- gets (randomR range)
+    put g
+    return x
+
+random_vec :: State StdGen Vec3
+random_vec = Vc3 <$> randomRS (-1, 1) <*> randomRS (-1, 1) <*> randomRS (-1, 1)
+
+random_vec_in_sphereS :: State StdGen Vec3
+random_vec_in_sphereS = do
+    vec <- random_vec
+    if length_sqr vec <= 1 then return vec
+    else random_vec_in_sphereS
+--head . filter (\vec -> length_sqr vec <= 1) <$> sequence [random_vec | i <- [1..]]
 \end{code}
